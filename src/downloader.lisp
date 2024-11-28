ca07543145634df00e0f4639d9156284cbfcf7ff
shartf <shartf@users.noreply.github.com>
2024-11-28 21:22:31 +0100

    Add drakma
    which also does not work

--- src/downloader.lisp
+++ src/downloader.lisp
@@ -1,10 +1,12 @@
 (defpackage #:aoc24/downloads
   (:use :cl)
-  (:local-nicknames (#:u :uiop)
-                    )
+  (:local-nicknames (#:u :uiop)		    
+		    #:drakma)
   (:import-from :uiop #:run-program #:directory-files #:subdirectories)
   (:export #:get-input))
 (in-package #:aoc24/downloads)
 
 (defun get-input ()
   (list '(1 2 4)))
+
+(drakma:http-request "https://adventofcode.com/2023/day/1/input") ;;zhopa
